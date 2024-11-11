d58276ff4aaf3d11dc9a755fcfc3b75ca2ff0ca7
Victor Kikuti <victor.kikuti@gmail.com>
2024-11-11 11:47:09 -0300

    usando api de cep para conseguir endereco por cep

--- forms/src/app/template-form/template-form.component.ts
+++ forms/src/app/template-form/template-form.component.ts
@@ -1,21 +1,30 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { FormDebugComponent } from '../form-debug/form-debug.component';
 import { CampoControlErroComponent } from '../campo-control-erro/campo-control-erro.component';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-template-form',
   standalone: true,
-  imports: [FormsModule, CommonModule, FormDebugComponent, CampoControlErroComponent],
+  imports: [FormsModule, CommonModule, FormDebugComponent, CampoControlErroComponent,
+    HttpClientModule],
   templateUrl: './template-form.component.html',
   styleUrl: './template-form.component.scss'
 })
-export class TemplateFormComponent {
+export class TemplateFormComponent implements OnInit{
 
   usuario: any = {
     nome: null,
     email: null
+  }
+
+  constructor(private http: HttpClient) { }
+
+  ngOnInit(): void {
+
   }
 
   onSubmit(form: any) {
@@ -29,6 +38,24 @@ export class TemplateFormComponent {
   aplicaCssErro(campo: any) {
     return {
       'is-invalid': this.verificaValidTouched(campo)
+    }
+  }
+
+  consultarCEP(cep: any) {
+    //Nova variável "cep" somente com dígitos
+    cep = cep.replace(/\D/g, '');
+
+    //Verifica se campo cep possui valor informado.
+    if(cep != "") {
+      var validacep = /^[0-9]{8}$/;
+
+      if(validacep.test(cep)) {
+
+        this.http.get(`//viacep.com.br/ws/${cep}/json`)
+        .pipe(map((response: any) => response))
+        .subscribe(response => console.log(response));
+
+      }
     }
   }
 
