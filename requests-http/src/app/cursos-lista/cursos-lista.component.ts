f89a1860a8730060b0774cc3331dbdc86fe310cb
Victor Kikuti <victor.kikuti@gmail.com>
2024-11-18 11:20:09 -0300

    HTTP GET: listar registros

--- requests-http/src/app/cursos-lista/cursos-lista.component.ts
+++ requests-http/src/app/cursos-lista/cursos-lista.component.ts
@@ -1,12 +1,26 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
+import { CursosService } from './cursos.service';
+import { Curso } from './curso';
 
 @Component({
   selector: 'app-cursos-lista',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, FormsModule, RouterModule,],
   templateUrl: './cursos-lista.component.html',
-  styleUrl: './cursos-lista.component.scss'
+  styleUrl: './cursos-lista.component.scss',
+  preserveWhitespaces: true
 })
-export class CursosListaComponent {
+export class CursosListaComponent implements OnInit{
 
+  cursos: Curso[];
+
+  constructor(private service: CursosService) {}
+
+  ngOnInit() {
+      this.service.list()
+      .subscribe(dados => this.cursos = dados);
+  }
 }
---
+++ requests-http/src/app/cursos-lista/cursos.service.ts
@@ -0,0 +1,21 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Curso } from './curso';
+import { tap } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CursosService {
+
+  private readonly API = 'http://localhost:3000/cursos';
+
+  constructor(private http: HttpClient) { }
+
+  list() {
+    return this.http.get<Curso[]>(this.API)
+      .pipe(
+        tap(console.log)
+      );
+  }
+}
