8d341661f5e3ea0ef59f48378d535da264303329
Victor Kikuti <victor.kikuti@gmail.com>
2024-11-18 10:50:37 -0300

    Configurando o projeto

--- requests-http/src/app/app.component.ts
+++ requests-http/src/app/app.component.ts
@@ -1,10 +1,17 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterModule, RouterOutlet } from '@angular/router';
+import { FormsModule } from '@angular/forms'
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    RouterModule,
+    CommonModule,
+    FormsModule
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- requests-http/src/app/app.routes.ts
+++ requests-http/src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', pathMatch: 'full', redirectTo: 'cursos'},
+  { path: 'cursos', loadComponent: () => import('./cursos-lista/cursos-lista.component').then((c) => c.CursosListaComponent)}
+];
