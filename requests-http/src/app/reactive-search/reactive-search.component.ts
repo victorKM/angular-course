760eb6d3cee17ef5c617edb23d1d6779c1902d0b
Victor Kikuti <victor.kikuti@gmail.com>
2024-11-21 14:57:04 -0300

    Http: Passando Parâmetros na URL (HttpParams)

--- requests-http/src/app/reactive-search/reactive-search.component.ts
+++ requests-http/src/app/reactive-search/reactive-search.component.ts
@@ -1,17 +1,43 @@
+import { CommonModule } from '@angular/common';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Component } from '@angular/core';
-import { FormControl, ReactiveFormsModule } from '@angular/forms';
+import { FormControl, FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { map, Observable, tap } from 'rxjs';
 
 @Component({
   selector: 'app-reactive-search',
   standalone: true,
-  imports: [ReactiveFormsModule],
+  imports: [ReactiveFormsModule, FormsModule, CommonModule],
   templateUrl: './reactive-search.component.html',
   styleUrl: './reactive-search.component.scss',
 })
 export class ReactiveSearchComponent {
   queryField = new FormControl();
+  readonly SEARCH_URL = 'https://api.cdnjs.com/libraries';
+  results$: Observable<any>;
+  total: number;
+
+  constructor(private http: HttpClient) {}
 
   onSearch() {
-    console.log(this.queryField.value);
+    const fields = 'name,description,version,homepage';
+    let value = this.queryField.value;
+    if (value && (value = value.trim()) !== '') {
+      // pode usar esta forma
+      const params_ = {
+        search: value,
+        fields: fields,
+      };
+
+      // ou esta forma
+      let params = new HttpParams();
+      params = params.set('search', value);
+      params = params.set('fields', fields);
+
+      this.results$ = this.http.get(this.SEARCH_URL, { params }).pipe(
+        tap((res: any) => (this.total = res.total)),
+        map((res: any) => res.results)
+      );
+    }
   }
 }
