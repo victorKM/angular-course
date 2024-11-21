4ed03b43d99ed68992fe1eda5d4bbb9d1f3e48a7
Victor Kikuti <victor.kikuti@gmail.com>
2024-11-21 10:40:57 -0300

    Upload de Arquivo: Back-end com Node.js

---
+++ requests-http/server/src/index.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const cors = require("cors");
+const multipart = require("connect-multiparty");
+
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+const corsOptions = {
+  origin: "*",
+  optionSuccessStatus: 200,
+};
+app.use(cors(corsOptions));
+
+const multipartMiddleware = multipart({ uploadDir: "./uploads" });
+app.post("/upload", multipartMiddleware, (red, res) => {
+  const files = req.files;
+  console.log(files);
+  res.json({ message: files });
+});
+
+app.use((err, req, res, next) => res.json({ error: err.message }));
+
+app.listen(8000, () => {
+  console.log("Servidor porta 8000");
+});
