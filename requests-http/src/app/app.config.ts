8a96a4d20fbb4a7e9fdf6b67387caa75ed4b2495
Victor Kikuti <victor.kikuti@gmail.com>
2024-11-22 13:38:43 -0300

    Deploy em Producao com Firebase e Gerando Imagem Docker com Build de Producao

--- requests-http/src/app/app.config.ts
+++ requests-http/src/app/app.config.ts
@@ -4,7 +4,9 @@ import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { provideHttpClient } from '@angular/common/http';
 import { provideAnimations } from '@angular/platform-browser/animations';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getRemoteConfig, provideRemoteConfig } from '@angular/fire/remote-config';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideHttpClient(), provideAnimations()]
+  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideHttpClient(), provideAnimations(), provideFirebaseApp(() => initializeApp({"projectId":"curso-angular-victork","appId":"1:220420474610:web:aea5359986599b8fe8726b","storageBucket":"curso-angular-victork.firebasestorage.app","apiKey":"AIzaSyCsaM4uSQz8ttICjOt_CNA5lM-89iRaGbQ","authDomain":"curso-angular-victork.firebaseapp.com","messagingSenderId":"220420474610"})), provideRemoteConfig(() => getRemoteConfig())]
 };
